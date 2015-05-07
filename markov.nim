79a00dc255545fe71c8c7e0c3d03f04869b35e04
Rob Bassi <rbassi@venasolutions.com>
2015-05-07 14:05:20 -0400

    Added comments

--- markov.nim
+++ markov.nim
@@ -1,16 +1,19 @@
-# read a file and construct a markov chain for word probabilities
+# Reads a file and construct a 2 word prefix markov, which is then used to generate anew sentence.
+
 import random, tables, strutils
 
+# contains a table for prefix to suffix mapping, and
+# a sequence of the unique keys for random sampling
 type Chain = object
   table*: TableRef[string, seq[string]]
   keys*: seq[string]
 
+# iterate over all words in a file
 iterator words(filename: string): string =
   var
     file = open(filename)
     word = ""
     c: char
-
   while not endOfFile file:
     c = readChar(file)
     case c
@@ -23,22 +26,22 @@ iterator words(filename: string): string =
     else: discard
   yield word
 
+# build a chain from a file
 proc buildChain(filename: string): Chain =
   var
     chain = Chain(table: newTable[string, seq[string]](), keys: @[])
     prefix1, prefix2 = ""
   for word in words(filename):
     let key = prefix1 & ' ' & prefix2
-
     if not chain.table.hasKey key:
       chain.table[key] = @[]
       chain.keys.add key
-
     chain.table.mget(key).add(word)
     prefix1 = prefix2
     prefix2 = word
   return chain
 
+# given a chain, randomly select a key, and start appending key + suffix to a string
 proc generateSentence(chain: Chain; maxLen: int = 100): string =
   let
     keys = chain.keys
@@ -48,7 +51,6 @@ proc generateSentence(chain: Chain; maxLen: int = 100): string =
     len = 0
     key = keys[idx]
     sentence = key
-
   while (chain.table.hasKey key) and (len < maxLen):
     let
       suffixes = chain.table[key]
@@ -61,7 +63,8 @@ proc generateSentence(chain: Chain; maxLen: int = 100): string =
     len += 3
   return sentence
 
-var
-  chain = buildChain("source.txt")
+# create a chain from a txt file
+var chain = buildChain("source.txt")
 
+# generate a sentence (max 250 words)
 echo generateSentence(chain, maxLen = 250)
